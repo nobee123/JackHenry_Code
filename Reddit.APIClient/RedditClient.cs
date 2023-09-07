76313fef43dfad102eb094d23a3333994c16d73e
nobee <nobee@live.com>
2023-09-06 19:06:17 -0700

    Revert "Add http client"
    This reverts commit a2d12b4437d0f8cc0b794874fde6fa7132787b2a.

--- Reddit.APIClient/RedditClient.cs
+++ Reddit.APIClient/RedditClient.cs
@@ -1,7 +1,5 @@
 ﻿using Reddit.Models;
 using System.Collections.Generic;
-using System.Net.Http.Headers;
-using System.Net.Http;
 
 namespace Reddit.APIClient
 {
@@ -13,29 +11,7 @@ namespace Reddit.APIClient
     {      
         public IEnumerable<Post> RetrieveSubredditPosts(string subreddit)
         {
-            try
-            {
-                var requestMessage = new HttpRequestMessage
-                {
-                    Method = HttpMethod.Get,
-                    RequestUri = new Uri("https://oauth.reddit.com/r/funny/new?limit=100&after=t3_16al61c")
-                };
-
-                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjk0MDY3MzUzLjA3NDE3OSwiaWF0IjoxNjkzOTgwOTUzLjA3NDE3OSwianRpIjoiSUdTTHYwYXhJdjY3MjB2aVFpWVpodkFndEFDd0lRIiwiY2lkIjoiaDFLaHZINFRyM0YzRmZEX3laUGZpZyIsImxpZCI6InQyXzNyd29nZTBoIiwiYWlkIjoidDJfM3J3b2dlMGgiLCJsY2EiOjE1NTc4OTg4OTQ0NDQsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.VypuAz8Ryjw-zvdLFImBF_traObU9BrffHRgwILXJtonWaC-l-mms0aXOJn9J66CtOiHijMJUtphZQvulW_5RRIMOa7717JchQaJZqvquzY8MYA45yunzgHfRtUmRMTCtPRHCGg385No3AldA7Yu-TiO2YnB-xOjp7UYOQvx-258O2dg_zhXhl3m4vxrW10x6mQPgjPmWAct2uOo9ItqKr1UrSIj61iT2p8uZAcngwzGxXmKQeyHGPud90BZ0LFGKfH4nkQyJxjwSJH5YiC0hbjEK8Er3gpRm3Ve_NiHl19UNCgsPTlbsdJ4vwHb-JNdF9SY_5zt9xCI_l3XdI1XuA");
-                requestMessage.Headers.UserAgent.Add(new ProductInfoHeaderValue("test", "1"));
-                requestMessage.Content = new FormUrlEncodedContent(new Dictionary<string, string>
-                {
-                    { "limit","100" }
-        });
-                var response = await httpClient.SendAsync(requestMessage);
-
-                var testing = response.Content.ReadAsStringAsync();
-
-            }
-            catch (Exception ex)
-            {
-
-            }
+            throw new NotImplementedException();
         }
     }
 }
